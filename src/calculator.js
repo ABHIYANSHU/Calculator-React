373364d5b7286ee7ec9b29a2b3f1f72942b276d5
Abhinav Srivastava <as@scoar.in>
2020-11-09 09:56:00 +0530

    Init Repo

---
+++ src/calculator.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+function Test() {
+    return <h1>Test</h1>;
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Test from './calculator';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
   document.getElementById('root')
 );
